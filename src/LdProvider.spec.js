5cd161224c30d246a32639779a200bd4df21309d
Brian Chen <brian.chen@tabcorp.com.au>
2020-08-16 17:50:49 +1000

    add provider tests

---
+++ src/LdProvider.spec.js
@@ -0,0 +1,81 @@
+// @flow
+import * as React from 'react';
+import { render } from '@testing-library/react';
+
+import {
+  LdProvider,
+  useLdFlag,
+} from '.';
+
+describe('useLdFlag', () => {
+  it('renders nothing before loading', () => {
+    const fakeClient = {
+      on: () => {},
+      allFlags: jest.fn(() => ({})),
+    };
+
+    const { queryByTestId } = render(
+      <LdProvider
+        client={fakeClient}
+      >
+        <div data-testid="element" />
+      </LdProvider>,
+    );
+
+    expect(queryByTestId('element')).toBe(null);
+  });
+
+  it('renders tree if nothing has loaded and async mode is enabled', () => {
+    const fakeClient = {
+      on: () => {},
+      allFlags: jest.fn(() => ({})),
+    };
+
+    const { getByTestId } = render(
+      <LdProvider
+        client={fakeClient}
+        async
+      >
+        <div data-testid="test" />
+      </LdProvider>,
+    );
+
+    expect(getByTestId('test')).not.toBe(null);
+  });
+
+  it('updates the flags on change', () => {
+    const fakeClient = {
+      on: (key, callback) => {
+        if (key === 'ready') {
+          callback();
+        } else if (key === 'change') {
+          callback({
+            enableTest: true,
+          });
+        }
+      },
+      allFlags: jest.fn(() => ({})),
+    };
+
+    const App = () => {
+      const enableTest = useLdFlag('enableTest');
+
+      return (
+        <div data-testid="test">
+          {enableTest ? 'true' : 'false'}
+        </div>
+      );
+    };
+
+    const { getByTestId } = render(
+      <LdProvider
+        client={fakeClient}
+        async
+      >
+        <App />
+      </LdProvider>,
+    );
+
+    expect(getByTestId('test')).not.toBe(null);
+  });
+});
